ba5c82472113c8a413a60384fda4ded3015870f9
Hadiya Hope-Crichlow <hopecrichlow@live.com>
2015-10-05 13:20:02 -0400

    Time being displayed through JS

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//IIFE
+(function () {
+
+var clock = new Date();
+console.log(clock)
+
+var hours = clock.getHours ();
+console.log(hours)
+var minutes = clock.getMinutes ();
+console.log(minutes)
+var seconds = clock.getSeconds ();
+console.log(seconds)
+
+
+
+
+
+
+
+
+} ());
