a74f19a795cdf9afe5bc24e8d4ee20ef56c3e516
Hadiya Hope-Crichlow <hopecrichlow@live.com>
2015-10-05 14:56:09 -0400

    Clock on page

--- js/main.js
+++ js/main.js
@@ -1,21 +1,58 @@
 //IIFE
 (function () {
 
-var clock = new Date();
+//Create variable for starting time
+var now = new Date();
 console.log(clock)
 
-var hours = clock.getHours ();
-console.log(hours)
-var minutes = clock.getMinutes ();
-console.log(minutes)
-var seconds = clock.getSeconds ();
-console.log(seconds)
+
+
+function formatHours (dateObj){
+  var hours = dateObj.getHours();
+  return (hours < 10) ? '0' + hours : hours;
+};
+
+
+var formattedHours = formatHours(now);
+console.log('aaaa', formattedHours);
+
+function formatMinutes (dateObj){
+  var minutes = dateObj.getMinutes();
+  return (minutes < 10) ? '0' + minutes : minutes;
+};
+
+var formattedMinutes = formatMinutes(now);
+console.log('bbbb', formattedMinutes);
+
+function formatSeconds (dateObj){
+  var seconds = dateObj.getSeconds();
+  return (seconds < 10) ? '0' + seconds : seconds;
+};
+
+var formattedSeconds = formatSeconds(now);
+console.log('ccc', formattedSeconds);
 
 
 
+// for(var minutes; minutes < 61; minutes++){
+//   console.log(minutes);
+// };
+
+// for(var seconds; seconds < 61; seconds++){
+//   console.log(seconds);
+// };
 
 
+//Display
 
+var display = formattedHours + ':' + formattedMinutes + ':' + formattedSeconds;
+  console.log(display)
 
+var clock = document.querySelector('#clock');
+   console.log(clock);
+
+var textNode = document.createTextNode(display);
+
+clock.appendChild(textNode);
 
 } ());
