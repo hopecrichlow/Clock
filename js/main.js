446064c18c6874fe107aa31e0c6670166a685757
Hadiya Hope-Crichlow <hopecrichlow@live.com>
2015-10-06 13:12:27 -0400

    Figuring out what went wrong with the hours function

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 
 window.setInterval(function(){
 
-//Create variable for starting time
+//Create variable for starting time, make a new date object
 var now = new Date();
 console.log(clock)
 
@@ -16,6 +16,10 @@ function formatHours (dateObj){
 var formattedHours = formatHours(now);
 // console.log('aaaa', formattedHours);
 
+var hexHours = formattedHours.toString(16);
+// console.log(hexHours);
+
+
 //Format Minutes
 function formatMinutes (dateObj){
   var minutes = dateObj.getMinutes();
@@ -23,6 +27,10 @@ function formatMinutes (dateObj){
 };
 var formattedMinutes = formatMinutes(now);
 // console.log('bbbb', formattedMinutes);
+
+var hexMinutes = formattedMinutes.toString(16);
+// console.log(hexMinutes);
+
 
 //Format Seconds
 function formatSeconds (dateObj){
@@ -32,14 +40,28 @@ function formatSeconds (dateObj){
 var formattedSeconds = formatSeconds(now);
 // console.log('ccc', formattedSeconds);
 
+var hexSeconds = formattedSeconds.toString(16);
 
+// function secs (){
+//   return (hexSeconds <= 9 || hexSeconds === 'a' || hexSeconds === 'b' || hexSeconds === 'c' || hexSeconds === 'd' || hexSeconds === 'e' || hexSeconds === 'f' || hexSeconds === 'g') ? hexSeconds + '0' : hexSeconds;
+// };
+
+//   if (a <= hexSeconds <= f ) {
+//     return hexSeconds + '0';
+// } else {
+//     return hexSeconds; };
+console.log(hexHours);
+console.log(hexMinutes);
+console.log(hexSeconds);
+
+// function 
 
 //Display
-var currentTime = '#' + formattedHours + formattedMinutes + formattedSeconds;
+var currentTime = '#' + hexHours + hexMinutes + hexSeconds;
   console.log(currentTime);
 
 //var textNode= document.createTextNode(currentTime);
-  $('#clock').text(currentTime).css('background-color', currentTime);
+  $('#clock').text(currentTime).css('background-color',currentTime);
 }, 1000);
 
 } ());
